dc1cc99b5ca421d2b651b4f8f4ae8bd78688f3ee
allen.versfeld@gmail.com <allen.versfeld@gmail.com@b8611b5a-4f16-6a0f-da48-2c288591a910>
2012-06-05 21:09:23 +0000

    Added sitemap processor

---
+++ urllist.py
@@ -0,0 +1,39 @@
+import re
+import urllib
+import sys
+
+def parse_sitemap(url):
+  """Returns a list of urls for each individual page on a Google Sites website.
+  Input: URL of sitemap (<RootURL>/system/feeds/sitemap, probably)
+  """
+  print url
+  ufile = urllib.urlopen(url)
+  urlcontents = ufile.readlines()
+  urls = ''
+  for line in urlcontents:
+    if '<loc>' in line:
+      urls += line
+  messyurls = urls.split('</loc>')
+  urllist = list()
+  for line in messyurls:
+    match = re.search(r'<loc>(.+)', line)
+    if match:
+      urllist.append(match.group(1))
+  return urllist
+  
+
+
+args = sys.argv[1:]
+if len(args) != 2:
+  print "Usage: urlsitemap-url outputfile"
+  print "    example:  urllist http://sites.google.com/site/foobar/system/feeds/sitemap urls.txt"
+  sys.exit()
+url = args[0]
+outfile = args[1]
+ofile = open(outfile, 'w')
+urllist = parse_sitemap(url)
+print urllist
+for line in urllist:
+  url = line + '\n'
+  ofile.write(url)
+ofile.close()
